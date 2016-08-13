c1a432100ecc8894434e113d7e08dafd1dabb151
Alexander Mancevice <amancevice@cargometrics.com>
2016-08-13 11:58:35 -0400

    travis

--- setup.py
+++ setup.py
@@ -16,6 +16,9 @@ CLASSIFIERS = [
     "Topic :: Utilities",
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python"]
 
 
