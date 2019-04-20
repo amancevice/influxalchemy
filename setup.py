35c02ec7ac0343243a30c241ae7055f1247b585e
Alexander Mancevice <amancevice@cargometrics.com>
2019-04-20 08:54:25 -0400

    Relax requirements
    Revert the strict requirements of 37edfccb6e4d301876d0d42f4fbc8916b9116bfc

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
+import textwrap
 from setuptools import setup
 from setuptools import find_packages
-
-
-def requirements(path):
-    with open(path) as req:
-        return [x.strip() for x in req.readlines() if not x.startswith('-')]
-
 
 setup(
     author='amancevice',
     author_email='smallweirdnum@gmail.com',
     description='Interact with InfluxDB using SQLAlchemy-style syntax',
-    install_requires=requirements('requirements.txt'),
+    install_requires=[
+        'influxdb >= 5.0.0',
+        'pytz >= 2018.3',
+        'requests >= 2.20.0',
+        'six >= 1.11.0',
+    ],
     name='influxalchemy',
     packages=find_packages(exclude=['tests']),
     setup_requires=['setuptools_scm'],
