b5ea9f2c6edd02e40ed0294707191f51fd4e4e18
Alexander Mancevice <alexander.mancevice@hey.com>
2022-11-29 19:30:49 -0500

    ~> 0.2.7

--- influxalchemy/__init__.py
+++ influxalchemy/__init__.py
@@ -4,4 +4,4 @@ InfluxDB Alchemy.
 from influxalchemy.client import InfluxAlchemy  # noqa: F401
 from influxalchemy.measurement import Measurement  # noqa: F401
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
